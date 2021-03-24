70530c1ae1e4e8590338bfe0a3a93ca5fc5522cd
Adriana Saty <adrianabertelli@simlabsprd.onmicrosoft.com>
2021-03-24 09:55:58 -0300

    [refactor] create pages folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './componentes/home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
